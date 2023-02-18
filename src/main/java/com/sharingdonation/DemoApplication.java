f37e0900675ab747b9621d42beb8c44bd799ac70
nsunf <jun5013@naver.com>
2023-02-18 22:36:24 +0900

    스프링 시큐리티, 메인컨트롤러 layout 테스트

--- src/main/java/com/sharingdonation/DemoApplication.java
+++ src/main/java/com/sharingdonation/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.sharingdonation.demo;
+package com.sharingdonation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
