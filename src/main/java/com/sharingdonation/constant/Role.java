54f43f7004b6d5199274ca7243f88586b8b31cce
taeyoung <taeyoung@unitsoft.kr>
2023-02-20 19:08:55 +0900

    Revert "pom.xml 파일은 꼭 다운받아서 설치하시기 바랍니다."
    This reverts commit 777c43a418abe11f9c7a6801774271ab5cf1243a.

--- src/main/java/com/sharingdonation/constant/Role.java
+++
@@ -1,5 +0,0 @@
-package com.sharingdonation.constant;
-
-public enum Role {
-	USER, COM, ADMIN
-}
--- src/main/java/com/sharingdonation/controller/DonationController.java
+++ src/main/java/com/sharingdonation/controller/DonationController.java
@@ -1,23 +1,5 @@
 package com.sharingdonation.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequiredArgsConstructor
 public class DonationController {
-	@GetMapping(value ="/donation")
-	public String donation(Model model) {
-		model.addAttribute(model);
-		return "donation/donationForm";
-	}
-	
-	@PostMapping(value = "/donation")
-	public String donation() {
-		return "redirect:/";
-	}
+
 }
