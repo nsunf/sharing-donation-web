31021825d940c5656b8e7ebafd3e1d6930a7f98f
nsunf <jun5013@naver.com>
2023-02-20 10:23:36 +0900

    header, footer 수정

--- src/main/resources/static/js/layout1.js
+++ src/main/resources/static/js/layout1.js
@@ -1,12 +1,14 @@
 window.onload = () => {
-	setLayout();
+	// setLayout();
 }
+
+// window.addEventListener("resize", setLayout);
 
 function setLayout() {
 	const header = document.querySelector("header");
 	const footer = document.querySelector("footer");
 
-	const content = document.querySelector(".content");
+	const content = document.querySelector(".page-wrapper");
 	
 	const headerHeight = header.clientHeight;
 	const footerHeight = footer.clientHeight;
