21b1c8aa90a7b52bcb67ca66da86f1450fb1901e
github BeomJu <github gtmxhs@gmail.com>
2023-02-22 15:17:13 +0900

    BeomJu git reset & re pull

--- src/main/java/com/sharingdonation/repository/DonationHeart.java
+++
@@ -1,7 +0,0 @@
-package com.sharingdonation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonationHeart extends JpaRepository<DonationHeart, Long>  {
-	long countByDonationId(Long donationId);
-}
