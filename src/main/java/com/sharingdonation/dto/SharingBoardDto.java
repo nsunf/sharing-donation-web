f34c049c6f0f0d334d16b26cd45c342ed9cb35c8
ksol46 <doomee880406@naver.com>
2023-02-25 22:39:32 +0900

    d

--- src/main/java/com/sharingdonation/dto/SharingBoardDto.java
+++ src/main/java/com/sharingdonation/dto/SharingBoardDto.java
@@ -1,6 +1,5 @@
 package com.sharingdonation.dto;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.modelmapper.ModelMapper;
@@ -15,16 +14,16 @@ public class SharingBoardDto {
 	
 	private Long id; //식별 아이디
 	
-	private String reg_time; //글 등록 날짜
+	private String regTime; //글 등록 날짜
 	
-	//private String 
+	private String sharing_member; //나눔상품 등록한 사람
+	
+	private String story_member; //사연채택 된 사람
 	
 	private String subject; //글제목
 	
 	private String content; //상세내용
-	
-	private Integer sharing_board_comment_id; //댓글 식별 아이디
-	
+
 	private static ModelMapper modelMapper = new ModelMapper();
 	
 	//dto를 엔티티로 바꿈
@@ -34,7 +33,7 @@ public class SharingBoardDto {
 	//엔티티를 dto로 바꿈
 	public static SharingBoardDto of(SharingBoard sharingBoard) {
 		SharingBoardDto sharingBoardDto = modelMapper.map(sharingBoard, SharingBoardDto.class);
-		sharingBoardDto.setReg_time(sharingBoard.getRegTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		sharingBoardDto.setRegTime(sharingBoard.getRegTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 		return sharingBoardDto;
 	}
 	
--- src/main/java/com/sharingdonation/service/SharingBoardService.java
+++ src/main/java/com/sharingdonation/service/SharingBoardService.java
@@ -10,8 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.sharingdonation.dto.SharingBoardDto;
 import com.sharingdonation.entity.SharingBoard;
+import com.sharingdonation.entity.Story;
 import com.sharingdonation.repository.SharingBoardImgRepository;
 import com.sharingdonation.repository.SharingBoardRepository;
+import com.sharingdonation.repository.StoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class SharingBoardService {
 	private final SharingBoardRepository sharingBoardRepository;
 	private final SharingBoardImgRepository sharingBoardImgRepository;
+	private final StoryRepository storyRepository;
 
 	// 나눔완료 게시글 리스트 가져오기
 	@Transactional(readOnly = true)
@@ -44,7 +47,20 @@ public class SharingBoardService {
 		SharingBoard sharingBoard = sharingBoardRepository.findById(id).orElseThrow(EntityNotFoundException::new);
 		SharingBoardDto sharingBoardDto = SharingBoardDto.of(sharingBoard);
 		
+		String sharedPostNickname = sharingBoard.getSharing().getMember().getNickName();
+		sharingBoardDto.setSharing_member(sharedPostNickname);
+		
+		
+		Story story = storyRepository.findByIdAndChooseYn(id,"Y");
+		String chooseYStory	= story.getMember().getNickName();
+		sharingBoardDto.setStory_member(chooseYStory);
+		
+		
 		return sharingBoardDto;
 	}
-
+	
+	/*
+	// 나눔완료 게시글 댓글
+	public SharingBoard
+	*/
 }
