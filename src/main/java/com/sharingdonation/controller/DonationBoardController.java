b8fdaf6c03c7f40087e705c73af6dec99b70e74e
longsiru <sirulong89@gmail.com>
2023-03-01 16:32:06 +0900

    0301/오후
    기부완료 리스트 관리자 페이지90%,업데이트 50%

--- src/main/java/com/sharingdonation/controller/DonationBoardController.java
+++ src/main/java/com/sharingdonation/controller/DonationBoardController.java
@@ -4,6 +4,7 @@ package com.sharingdonation.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
@@ -22,12 +23,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import com.sharingdonation.dto.DonationBoardCommentDto;
 import com.sharingdonation.dto.DonationBoardDto;
 import com.sharingdonation.dto.DonationBoardFormDto;
 import com.sharingdonation.dto.DonationBoardImgDto;
+import com.sharingdonation.dto.DonationBoardSelectDto;
 import com.sharingdonation.dto.DonationFormDto;
 import com.sharingdonation.dto.SharingBoardCommentDto;
+import com.sharingdonation.entity.DonationBoard;
 import com.sharingdonation.entity.DonationBoardComment;
 import com.sharingdonation.repository.MemberRepository;
 import com.sharingdonation.service.DonationBoardHeartService;
@@ -81,9 +85,23 @@ public class DonationBoardController {
 			return "admin/editDonatedBoard";
 		}
 		
-		return "redirect:/donatedBoard";
+		return "redirect:/admin/donatedBoard";
 		
 	}
+	
+	//show donated board list page
+		@GetMapping(value = "/admin/donatedBoard")
+		public String AdmindonatedBoard(Optional<Integer> page, Model model) { 
+			
+			Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+			Page<DonationBoardDto> donationBoards = donationBoardService.getDonationBoardDtoPage(pageable);
+		
+			model.addAttribute("donationBoards",donationBoards);
+			model.addAttribute("donations", donationBoardService.getDonationBorardSelect());
+			model.addAttribute("maxPage", 5);
+			return "admin/donatedList";
+			
+		}
 	
 	//show donated board list page
 	@GetMapping(value = "/donatedBoard")
@@ -151,7 +169,51 @@ public class DonationBoardController {
 			return new ResponseEntity<Long>(donationBoardheartCount, HttpStatus.OK);
 		}
 		
+	
+	//doantionBoard modify view
+		@GetMapping(value = "/admin/donatedBoard/{donationBoardId}/modify")
+		public String donatedBoardDetail(@PathVariable("donationBoardId") Long donationBoardId, Model model) {
+			try {
+				DonationBoardFormDto donationBoardFormDto = donationBoardService.getdonationBoardDetail(donationBoardId);
+				model.addAttribute(donationBoardFormDto);
+			} catch (EntityNotFoundException e) {
+				model.addAttribute("errorMessage","This gamer that dose not exist." );
+				model.addAttribute("donationBoardFormDto", new DonationBoardFormDto());
+				model.addAttribute("donations", donationBoardService.getDonationBorardSelect());
+				return "admin/editDonatedBoard";
+			}
+			
+			return "admin/editDonatedBoard";
+			
+		}
 		
+		
+		//doantionBoard modify
+		@PostMapping(value = "/admin/donatedBoard/{donationBoardId}/")
+		public String updateDonationBoard(@Valid DonationBoardFormDto donationBoardformDto, BindingResult bindingResult,
+				Model model, @RequestParam("donationBoardImgFile") List<MultipartFile> donationBoardImgFileList) {
+			if(bindingResult.hasErrors()) {
+				return "admin/editDonatedBoard";
+			}
+			
+			
+			if(donationBoardImgFileList.get(0).isEmpty() && donationBoardformDto.getId() == null) {
+				model.addAttribute("errorMessage", "Must enter danation board image.");
+				return "admin/editDonatedBoard";
+			}
+			
+			try {
+				donationBoardService.donationBoardUpdate(donationBoardformDto, donationBoardImgFileList);
+			} catch (Exception e) {
+				e.printStackTrace();
+				model.addAttribute("errorMessage", "An error occurred while modifying.");
+				return "admin/editDonatedBoard";
+			}
+			
+			
+			return "redirect:/";
+			
+		}
 		
 	//delete
 		@DeleteMapping(value= "/donationBoard/{donationBoardId}/delete")
