c949f72e8a696637c79856d00a0ee6412b83d4ca
nsunf <jun5013@naver.com>
2023-03-10 09:58:47 +0900

    aws 배포 설정

--- src/main/java/com/sharingdonation/DemoApplication.java
+++ src/main/java/com/sharingdonation/DemoApplication.java
@@ -3,11 +3,18 @@ package com.sharingdonation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
 	}
 }
